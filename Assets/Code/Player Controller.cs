c25ca0cd9717116bbb3c84c6952287e9549f569a
suguetsu <amarelo_77@hotmail.com>
2020-12-19 17:02:28 -0300

    First commit

---
+++ Assets/Code/Player Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private RididyBody2D PlayerRb;
+
+    public float Speed;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRb = getcomponent<RididyBody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void Move()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CartoonFly
+Study
