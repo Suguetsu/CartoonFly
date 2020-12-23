0a34b0287f4517b6837e9a4c38f8a516a2f5e581
suguetsu <amarelo_77@hotmail.com>
2020-12-22 23:25:09 -0300

    Second Commit

--- Assets/Code/EnemyController.cs
+++ Assets/Code/EnemyController.cs
@@ -9,15 +9,26 @@ public class EnemyController : MonoBehaviour
     public GameObject blowPrefa;
     private GameController _GC;
 
+    private PlayerController _Pcontrol;
+
+    public Transform Weapon;
+    public GameObject ShotPrefab;
+
     void Start()
     {
         _GC = FindObjectOfType(typeof(GameController)) as GameController;
+        _Pcontrol = FindObjectOfType(typeof(PlayerController)) as PlayerController;
+
+        StartCoroutine("ShotTime");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Weapon.right = _Pcontrol.transform.position - transform.localPosition; 
+        // diferença de vetores para achar a posição do player
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -32,9 +43,23 @@ public class EnemyController : MonoBehaviour
                 Destroy(temp, 0.5f);
 
                 _GC.SpawnLoot(this.transform);
-              
+
                 break;
-        
+
         }
+    }
+
+    private void Shot()
+    {
+        GameObject temp = Instantiate(ShotPrefab, Weapon.position, Weapon.localRotation);
+        temp.GetComponent<Rigidbody2D>().velocity = Weapon.right * _GC.shotvelocity;
+
+    }
+
+    IEnumerator ShotTime()
+    {
+        yield return new WaitForSeconds(Random.Range(_GC.timeToShot[0], _GC.timeToShot[1]));
+        Shot();
+        StartCoroutine("ShotTime");
     }
 }
