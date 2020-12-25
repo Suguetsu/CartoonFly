1f8fc33db0317d2c08435c0daf72dc59681be174
suguetsu <amarelo_77@hotmail.com>
2020-12-24 21:58:53 -0300

    Third Commit

--- Assets/Code/CamController.cs
+++ Assets/Code/CamController.cs
@@ -7,68 +7,70 @@ public class CamController : MonoBehaviour
     // Start is called before the first frame update
     private GameController _GC;
 
-    [SerializeField]
-    public Transform playerPos;
 
 
     void Start()
     {
         _GC = FindObjectOfType(typeof(GameController)) as GameController;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        CamOffset();
+        if (_GC.isAlivePlayer && _GC.playerTransform.transform.position != null)
+            CamOffset();
     }
 
     private void CamOffset()
     {
 
-        float posPlayerX = playerPos.position.x;
-        float posPlayerY = playerPos.position.y;
+        float posPlayerX = _GC.playerTransform.transform.position.x;
+        float posPlayerY = _GC.playerTransform.transform.position.y;
 
 
-        if (playerPos.position.x >= _GC.limiDir.position.x)
+        if (_GC.playerTransform.transform.position.x >= _GC.limiDir.position.x)
         {
-            playerPos.position = new Vector2(_GC.limiDir.position.x, playerPos.position.y);
+            _GC.playerTransform.transform.position = new Vector2(_GC.limiDir.position.x, _GC.playerTransform.transform.position.y);
         }
-        else if (playerPos.position.x <= _GC.limiEsq.position.x)
+        else if (_GC.playerTransform.transform.position.x <= _GC.limiEsq.position.x)
         {
-            playerPos.position = new Vector2(_GC.limiEsq.position.x, playerPos.position.y);
+            _GC.playerTransform.transform.position = new Vector2(_GC.limiEsq.position.x, _GC.playerTransform.transform.position.y);
 
         }
 
-        if (playerPos.position.y >= _GC.limSup.position.y)
+        if (_GC.playerTransform.transform.position.y >= _GC.limSup.position.y)
         {
-            playerPos.position = new Vector2(playerPos.position.x, _GC.limSup.position.y);
+            _GC.playerTransform.transform.position = new Vector2(_GC.playerTransform.transform.position.x, _GC.limSup.position.y);
         }
-        else if (playerPos.position.y <= _GC.limInf.position.y)
+        else if (_GC.playerTransform.transform.position.y <= _GC.limInf.position.y)
         {
-            playerPos.position = new Vector2(playerPos.position.x, _GC.limInf.position.y);
+            _GC.playerTransform.transform.position = new Vector2(_GC.playerTransform.transform.position.x, _GC.limInf.position.y);
 
         }
     }
 
     private void LateUpdate()
     {
-        if (transform.position.x > _GC.limiCamEsq.position.x && transform.position.x < _GC.limiCamDir.position.x)
+        if (_GC.isAlivePlayer && _GC.playerTransform.transform.position != null)
         {
-            CamLimited();
-        }
-        else if (transform.position.x <= _GC.limiCamEsq.position.x && playerPos.position.x > _GC.limiCamEsq.position.x)
-        {
-            CamLimited();
-        }
-        else if (transform.position.x >= _GC.limiCamDir.position.x && playerPos.position.x < _GC.limiCamDir.position.x)
-        {
-            CamLimited();
+            if (transform.position.x > _GC.limiCamEsq.position.x && transform.position.x < _GC.limiCamDir.position.x)
+            {
+                CamLimited();
+            }
+            else if (transform.position.x <= _GC.limiCamEsq.position.x && _GC.playerTransform.transform.position.x > _GC.limiCamEsq.position.x)
+            {
+                CamLimited();
+            }
+            else if (transform.position.x >= _GC.limiCamDir.position.x && _GC.playerTransform.transform.position.x < _GC.limiCamDir.position.x)
+            {
+                CamLimited();
+            }
+
         }
     }
     void CamLimited()
     {
-        Vector3 pos = new Vector3(playerPos.position.x, transform.position.y, -10);
+        Vector3 pos = new Vector3(_GC.playerTransform.transform.position.x, transform.position.y, -10);
         transform.position = Vector3.Lerp(transform.position, pos, _GC.velCam * Time.deltaTime);
 
     }
--- Assets/Code/GameController.cs
+++ Assets/Code/GameController.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TagShot
+{
+    playerShot, enemyShot
+}
+
 public class GameController : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -13,13 +18,32 @@ public class GameController : MonoBehaviour
     public float timeToMove;
     public int shotvelocity;
     public int[] timeToShot;
+    public int idShotEnemy;
     public GameObject[] enemyPrefab;
+
+    [Header("Explosion Prefab")]
+    public GameObject explosion;
+
+    [Header("Shoot prefab")]
+    public GameObject[] shotPrefab;
 
     [Header("Player config")]
     public Transform limSup;
     public Transform limInf;
     public Transform limiEsq;
     public Transform limiDir;
+    public Transform spawnPlayer;
+
+    public int extraLife;
+    public int idShotPlayer;
+    [SerializeField]
+    public PlayerController _PlayerC;
+    public bool isAlivePlayer;
+    public bool isInvunerable;
+    public float timeInvulnerable;
+
+    public GameObject playerPrefab;
+    public Transform playerTransform;
 
     [Header("Cam config")]
     public Transform limiCamEsq;
@@ -29,7 +53,12 @@ public class GameController : MonoBehaviour
 
     void Start()
     {
-        
+
+        //if (isAlivePlayer)
+        //_PlayerC = FindObjectOfType(typeof(PlayerController)) as PlayerController;
+
+
+
     }
 
     // Update is called once per frame
@@ -39,7 +68,7 @@ public class GameController : MonoBehaviour
     }
 
     // reponsavel por gerenciar a chance de pegar loot
-   public void SpawnLoot(Transform enemy)
+    public void SpawnLoot(Transform enemy)
     {
         int chance = Random.Range(0, 100);
         int index;
@@ -49,12 +78,13 @@ public class GameController : MonoBehaviour
             int percent = Random.Range(0, 100);
 
 
-            if(percent > 85)
+            if (percent > 85)
             {
                 index = 2;
-                
+
                 // bomb
-            }else if(percent > 65)
+            }
+            else if (percent > 65)
             {
                 index = 1;
             }
@@ -65,5 +95,104 @@ public class GameController : MonoBehaviour
 
             Instantiate(powerUps[index].transform, enemy.position, enemy.transform.localRotation);
         }
+    }
+
+    public string AplicarTag(TagShot tag)
+    {
+
+        string retorno = null;
+
+        switch (tag)
+        {
+            case TagShot.playerShot:
+                retorno = "playerShot";
+                break;
+            case TagShot.enemyShot:
+                retorno = "enemyShot";
+                break;
+
+
+        }
+        return retorno;
+    }
+
+
+    public IEnumerator TimetoRespawn()
+    {
+        isAlivePlayer = false;
+
+
+        yield return new WaitForSeconds(1);
+        PlayerDeath();
+    }
+
+
+
+    public void PlayerDeath()
+    {
+        extraLife -= 1;
+
+        if (extraLife >= 0)
+        {
+            GameObject temp = Instantiate(playerPrefab, spawnPlayer.position, spawnPlayer.localRotation);
+            playerTransform = temp.transform;
+            temp.GetComponent<Collider2D>().enabled = false;
+
+            StartCoroutine(Invunerable(temp));
+
+        }
+        else
+        {
+
+
+            print("Game Over");
+        }
+
+    }
+
+    public IEnumerator Invunerable(GameObject tempPlayer)
+    {
+
+        float a = Mathf.PingPong(0.5f, 1);
+        Debug.Log("color: " + a);
+        Color b = new Color(200, 200, 200, a);
+        tempPlayer.GetComponent<SpriteRenderer>().color = b;
+
+        isInvunerable = true;
+        StartCoroutine(Piscado(tempPlayer));
+
+
+        yield return new WaitForSeconds(timeInvulnerable);
+
+        isInvunerable = false;
+        b = new Color(255, 255, 255, 1);
+        tempPlayer.GetComponent<SpriteRenderer>().color = b;
+        tempPlayer.GetComponent<Collider2D>().enabled = true;
+
+
+    }
+
+    public IEnumerator Piscado(GameObject tempPlayer2)
+    {
+        if (isInvunerable)
+        {
+            Debug.Log("executando");
+            yield return new WaitForSeconds(0.25f);
+            if (tempPlayer2 != null)
+            {
+                tempPlayer2.GetComponent<SpriteRenderer>().enabled = !tempPlayer2.GetComponent<SpriteRenderer>().enabled;
+
+                StartCoroutine(Piscado(tempPlayer2));
+            }
+        }
+        else
+        {
+            yield return new WaitForEndOfFrame();
+            StopCoroutine(Piscado(tempPlayer2));
+            tempPlayer2.GetComponent<SpriteRenderer>().enabled = true;
+            Debug.Log("parou??");
+        }
+
+
     }
 }
